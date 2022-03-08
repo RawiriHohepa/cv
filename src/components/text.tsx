40c2925b6f7211cba64a28255553c84e5b41d555
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2022-03-08 17:46:42 +1300

    Convert text components to use css modules

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,78 +1,39 @@
-import styled from "styled-components"
+import React from "react";
+import styles from "./text.module.css";
 
-export const Title = styled.h1`
-    margin: 0;
-    font-weight: bold;
-`
-export const SubTitle = styled.h1`
-    margin: 0;
-    font-weight: normal;
-    color: #686868;
-`
-export const SectionTitle = styled.h2`
-    margin: 2rem 0 1rem 0;
-    font-weight: normal;
-    color: #686868;
-`
-export const Heading = styled.h3`
-    margin: 0;
-    font-weight: bold;
+type TextProps = {
+    children: React.ReactNode;
+    size?: "small" | "regular";
+    style?: any;
+}
+const defaultSize = "regular";
 
-    ${({ size }: { size?: "regular" | "small" }) => {
-        if (size === "small") {
-            return `
-                font-size: 1rem;
-            `
-        }
-    }}
-`
-export const SubHeading = styled.h3`
-    margin: 0;
-    font-weight: normal;
+export const Title = (props: TextProps) => 
+    <h1 className={styles.title} style={props.style}>{props.children}</h1>
 
-    ${({ size }: { size?: "regular" | "small" }) => {
-        if (size === "small") {
-            return `
-            font-size: 1rem;
-            `
-        }
-    }}
-`
-export const Description = styled.h4`
-    margin: 0;
-    font-weight: normal;
-    color: #686868;
+export const SubTitle = (props: TextProps) => 
+    <h1 className={styles.subTitle} style={props.style}>{props.children}</h1>
 
-    ${({ size }: { size?: "regular" | "small" }) => {
-        if (size === "small") {
-            return `
-                font-size: 0.8rem;
-            `
-        }
-    }}
-`
-export const Details = styled.ul`
-    margin: 0;
-    font-weight: normal;
-`
-export const DetailsItem = styled.li`
-    margin: 0;
-    font-weight: normal;
-    color: #4B4B4B;
+export const SectionTitle = (props: TextProps) => 
+    <h2 className={styles.sectionTitle} style={props.style}>{props.children}</h2>
 
-    ${({ size }: { size?: "regular" | "small" }) => {
-        if (size === "small") {
-            return `
-                font-size: 0.8rem;
-            `
-        }
-    }}
-`
-export const Text = styled.p`
-    margin: 0 0 0.25rem 0;
-    font-weight: normal;
-`
+export const Heading = (props: TextProps) => 
+    <h3 className={`${styles.heading} ${styles[props.size || defaultSize]}`} style={props.style}>{props.children}</h3>
 
-export const Bold = styled.span`
-    font-weight: bold;
-`
+export const SubHeading = (props: TextProps) => 
+    <h3 className={`${styles.subHeading} ${styles[props.size || defaultSize]}`} style={props.style}>{props.children}</h3>
+
+export const Description = (props: TextProps) => 
+    <h4 className={`${styles.description} ${styles[props.size || defaultSize]}`} style={props.style}>{props.children}</h4>
+
+export const Details = (props: TextProps) => 
+    <ul className={styles.details} style={props.style}>{props.children}</ul>
+
+export const DetailsItem = (props: TextProps) => 
+    <li className={`${styles.detailsItem} ${styles[props.size || defaultSize]}`} style={props.style}>{props.children}</li>
+
+export const Text = (props: TextProps) => 
+    <p className={styles.text} style={props.style}>{props.children}</p>
+
+export const Bold = (props: TextProps) => 
+    <span className={styles.bold} style={props.style}>{props.children}</span>
